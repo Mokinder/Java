4638436d528af8e7301ac18bcb2127bae1a49869
Mokinder <83770793+Mokinder@users.noreply.github.com>
2022-04-08 13:47:14 +0530

    Add files via upload

---
+++ Practical/arrayex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class arrayex
+{
+public static void main(String args[])
+{
+String names[]={"akash","sanjay","murali","prakesh"};
+for(int i=0;i<names.length;i++)
+{
+System.out.println(names[i]);
+}
+System.out.println();
+int x[]={1,2,3,4,5,6,7};
+for(int i=0;i<x.length;i++)
+{
+System.out.println(x[i]);
+}
+}
+}
