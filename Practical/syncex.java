2f264732f09fad07f172ea16e0c1999ec957ebdf
Mokinder <83770793+Mokinder@users.noreply.github.com>
2022-05-05 12:19:24 +0530

    Add files via upload

---
+++ Practical/syncex.java
@@ -0,0 +1,54 @@
+class sample
+{
+void print(int num)
+{
+for(int i=1;i<=5;i++)
+{
+System.out.println(num*i);
+try{
+Thread.sleep(500);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
+}
+class Thread1 extends Thread
+{
+sample t;
+public Thread1(sample t)
+{
+this.t=t;
+}
+public void run()
+{
+t.print(3);
+}
+}
+
+class Thread2 extends Thread
+{
+sample t;
+public Thread2(sample t)
+{
+this.t=t;
+}
+public void run()
+{
+t.print(5);
+}
+}
+
+public class syncex
+{
+public static void main(String args[])
+{
+sample s=new sample();
+Thread1 t=new Thread1(s);
+Thread2 t1=new Thread2(s);
+t.start();
+t1.start();
+}
+}
